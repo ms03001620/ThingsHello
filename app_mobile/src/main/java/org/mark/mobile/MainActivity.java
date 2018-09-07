e247c0d863b908f542ca34a674ae78eca6b9ab36
mazhenjin <mazhenjin@baidu.com>
2018-09-07 15:35:19 +0800

    客户端显示版本号为title

--- app_mobile/src/main/java/org/mark/mobile/MainActivity.java
+++ app_mobile/src/main/java/org/mark/mobile/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Version: " + BuildConfig.VERSION_NAME);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         editTextHost = findViewById(R.id.edit_host);
