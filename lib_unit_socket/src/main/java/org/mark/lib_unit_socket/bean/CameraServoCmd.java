556bd479cd3a7e773d144ece0ea1615740a41f2a
Mark <mazhenjin@sina.com>
2020-04-28 03:35:00 +0800

    舵机cmd控制命令调整

--- lib_unit_socket/src/main/java/org/mark/lib_unit_socket/bean/CameraServoCmd.java
+++ lib_unit_socket/src/main/java/org/mark/lib_unit_socket/bean/CameraServoCmd.java
@@ -2,27 +2,13 @@ package org.mark.lib_unit_socket.bean;
 
 
 public class CameraServoCmd {
-    private int horizontal;
-    private int vertical;
+    private int progress;
 
-    public CameraServoCmd(int horizontal, int vertical) {
-        this.horizontal = horizontal;
-        this.vertical = vertical;
+    public CameraServoCmd(int progress) {
+        this.progress = progress;
     }
 
-    public int getHorizontal() {
-        return horizontal;
-    }
-
-    public void setHorizontal(int horizontal) {
-        this.horizontal = horizontal;
-    }
-
-    public int getVertical() {
-        return vertical;
-    }
-
-    public void setVertical(int vertical) {
-        this.vertical = vertical;
+    public int getProgress() {
+        return progress;
     }
 }
